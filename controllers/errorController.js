9bda523b136a75d5ffec5c29c208f8c368125566
Akhil <bts.akhil@gmail.com>
2021-10-17 16:31:59 +0530

    Edited design

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,45 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or other unkown error
+  else {
+    // Log the error
+    console.error("Error💥 ", err);
+    // Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    if (err.name === "CastError") handleCastErrorDB(err);
+
+    sendErrorProd(err, res);
+  }
 
   res.status(err.statusCode).json({
     status: err.status,
--- public/js/script.js
+++ public/js/script.js
@@ -15,21 +15,25 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    // e.preventDefault();
-    // const href = link.getAttribute("href");
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-    // // Scroll back to top
-    // if (href === "#")
-    //   window.scrollTo({
-    //     top: 0,
-    //     behavior: "smooth",
-    //   });
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
 
-    // // Scroll to other links
-    // if (href !== "#" && href.startsWith("#")) {
-    //   const sectionEl = document.querySelector(href);
-    //   sectionEl.scrollIntoView({ behavior: "smooth" });
-    // }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (!href.startsWith("#")) {
+      window.open(href);
+    }
 
     // Close mobile naviagtion
     if (link.classList.contains("main-nav-link")) {
