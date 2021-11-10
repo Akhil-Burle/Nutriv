0d8216a27c06be2673617be699fd56ce8f661f10
Akhil <bts.akhil@gmail.com>
2021-11-10 19:37:40 +0530

    Successfully solved CSP hemet error

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ app.set("views", path.join(__dirname, "./views"));
 // 1) GLOBAL MIDDLEWARES
 
 // Security HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // Development loggin
 if (process.env.NODE_ENV === "development") {
