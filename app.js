7ddd82952bd8f2da8d1a0808c94caad7aa04ff7e
Akhil <bts.akhil@gmail.com>
2021-11-17 12:17:36 +0530

    Done Moving to dev

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use("/api", limiter);
 
 app.post(
   "/webhook-checkout",
-  express.raw({ type: "application/json" }),
+  bodyParser.raw({ type: "application/json" }),
   bookingController.webhookCheckout
 );
 
