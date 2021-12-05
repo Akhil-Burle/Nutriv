569c14494595d7576b41be4b8a909041be1beee8
Akhil <bts.akhil@gmail.com>
2021-12-05 11:34:55 +0530

    Testing price checkout stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -41,7 +41,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const dish = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.data.object.amount_total / 100;
   await Booking.create({ dish, user, price });
 };
 
